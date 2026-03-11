9c6e5da3cdeba846e3d698093fe2eb9545303f16
Mykhailo Voznesenskyi <mykhailo.voznesenskyi@amedia.ua>
2026-03-11 10:57:48 +0200

    fix: database naming strategy

--- src/db/migrations.config.ts
+++ src/db/migrations.config.ts
@@ -1,6 +1,8 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 
+import { DatabaseNamingStrategy } from 'src/db/naming.strategy';
+
 dotenv.config();
 
 export default new DataSource({
@@ -8,5 +10,6 @@ export default new DataSource({
   url: process.env.DATABASE_URL,
   entities: [__dirname + '/../modules/**/entities/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  namingStrategy: new DatabaseNamingStrategy(),
   synchronize: false,
 });
--- src/db/migrations/1773175248090-UserTable.ts
+++ src/db/migrations/1773175248090-UserTable.ts
@@ -5,15 +5,15 @@ export class UserTable1773175248090 implements MigrationInterface {
 
   async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TYPE "public"."user_entity_role_enum" AS ENUM('guest', 'user')`,
+      `CREATE TYPE "public"."user_role_enum" AS ENUM('guest', 'user')`,
     );
     await queryRunner.query(
-      `CREATE TABLE "user_entity" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying NOT NULL, "password" character varying NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "role" "public"."user_entity_role_enum" NOT NULL DEFAULT 'user', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, CONSTRAINT "UQ_415c35b9b3b6fe45a3b065030f5" UNIQUE ("email"), CONSTRAINT "PK_b54f8ea623b17094db7667d8206" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying NOT NULL, "password" character varying NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "role" "public"."user_role_enum" NOT NULL DEFAULT 'user', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, CONSTRAINT "UQ_415c35b9b3b6fe45a3b065030f5" UNIQUE ("email"), CONSTRAINT "PK_b54f8ea623b17094db7667d8206" PRIMARY KEY ("id"))`,
     );
   }
 
   async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "user_entity"`);
-    await queryRunner.query(`DROP TYPE "public"."user_entity_role_enum"`);
+    await queryRunner.query(`DROP TABLE "user"`);
+    await queryRunner.query(`DROP TYPE "public"."user_role_enum"`);
   }
 }
